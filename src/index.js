e63953a6973aff6f4cda24643e48c6468cfa8a7d
case <case@casesandberg.com>
2017-04-12 18:17:13 -0700

    Is Package Up to Date

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import request from 'request'
+
+export const isPackageUpToDate = ({ name, version }) => new Promise((resolve, reject) {
+  request(`http://registry.npmjs.org/${ name }`, (err, res) => {
+    err && reject(err)
+
+    if (res['dist-tags'].latest === version) {
+      resolve()
+    } else {
+      reject()
+    }
+  })
+});
